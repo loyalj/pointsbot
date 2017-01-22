734c5d69ead373cf4f8a5b926ddb8ac3605a90ec
loyalj <loyalj@gmail.com>
2017-01-22 15:25:36 -0500

    typo

--- scripts/load_db.sql
+++ scripts/load_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE awards (
-    'action'     varchar(40) CONSTRAINT firstkey PRIMARY KEY,
-    'from'       varchar(40) NOT NULL,
-    'to'         varchar(40) NOT NULL,
-    'award'      varchar(40),
-    'value'      integer NOT NULL
+    "action"     varchar(40) CONSTRAINT firstkey PRIMARY KEY,
+    "from"       varchar(40) NOT NULL,
+    "to"         varchar(40) NOT NULL,
+    "award"      varchar(40),
+    "value"      integer NOT NULL
 );
