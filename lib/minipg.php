802431ff2ccff50bee8a68b4f8041d1d76e8638f
loyalj <loyalj@gmail.com>
2017-01-22 09:05:10 -0500

    db connect

--- lib/minipg.php
+++ lib/minipg.php
@@ -23,18 +23,18 @@ class MiniPG {
         $this->host = $parsedUrl['host'];
         $this->path = $path;
 
-        $this->connectString = "user={$this->user} password={$this->pass} host={$this->host} dbname=" . substr($path, 1);
+        $this->connectString = "user={$this->user} password={$this->password} host={$this->host} dbname=" . substr($path, 1);
         echo $this->connectString;
 	}
 
 
     public function testConnection() {
-        //$pgConn = pg_connect($this->connectString);
-        //$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+        $pgConn = pg_connect($this->connectString);
+        $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
-        //if (!pg_num_rows($result)) {
-        //    echo "Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n";
-        //}
+        if (!pg_num_rows($result)) {
+            echo "\n\nYour connection is working, but your database is empty.\nFret not. This is expected for new apps.\n";
+        }
 
     }
 	
