a16acaba398fe6a0d608a6661ea120846faf4324
loyalj <loyalj@gmail.com>
2017-01-22 09:15:57 -0500

    db connect

--- lib/minipg.php
+++ lib/minipg.php
@@ -5,14 +5,15 @@ class MiniPG {
 	private $user = null;
 	private $password = null;
 	private $host = null;
-	private $path = null;
+    private $port = null;
+	private $database = null;
     private $connectString = null;
 
 	function __construct($databaseUrl) {
-        $url = substr($databaseUrl, 0, strrpos($databaseUrl, ':'));
+        $url = substr($databaseUrl, 0, strrpos($databaseUrl, '/'));
         echo $url . "\n\n";
-        $path = substr($databaseUrl, strrpos($databaseUrl, ':') );
-        echo $path . "\n\n" ;
+        $database = substr($databaseUrl, strrpos($databaseUrl, '/'));
+        echo $database . "\n\n" ;
 
         $parsedUrl = parse_url($url);
         print_r($parsedUrl);
@@ -21,20 +22,21 @@ class MiniPG {
         $this->user = $parsedUrl['user'];
         $this->password = $parsedUrl['pass'];
         $this->host = $parsedUrl['host'];
-        $this->path = $path;
+        $this->port = $parsedUrl['port'];
+        $this->database = $database;
 
-        $this->connectString = "user={$this->user} password={$this->password} host={$this->host} dbname=" . substr($path, 1);
+        $this->connectString = "user={$this->user} password={$this->password} host={$this->host} port={$this->port} dbname={$this->database}";
         echo $this->connectString;
 	}
 
 
     public function testConnection() {
-        $pgConn = pg_connect($this->connectString);
+        /*$pgConn = pg_connect($this->connectString);
         $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
         if (!pg_num_rows($result)) {
             echo "\n\nYour connection is working, but your database is empty.\nFret not. This is expected for new apps.\n";
-        }
+        }*/
 
     }
 	
