81a9726fcb11ff57a36b44afbd0faaa975974757
loyalj <loyalj@gmail.com>
2017-01-28 11:45:55 -0500

    DB updates

--- scripts/load_db.sql
+++ scripts/load_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE awards (
     "action"     varchar(40),
-    "from"       varchar(40),
-    "to"         varchar(40),
+    "from_user"  varchar(40),
+    "to_user"    varchar(40),
     "award"      varchar(40),
     "value"      integer
 );
