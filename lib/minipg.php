488ca7bedc84911a402a801393158c207ad085f4
loyalj <loyalj@gmail.com>
2017-01-29 17:52:33 -0500

    db debug

--- lib/minipg.php
+++ lib/minipg.php
@@ -58,11 +58,11 @@ class MiniPG {
     public function getUserStats($user) {
         //SELECT to_user, award, sum(value) as tval FROM awards where to_user = '<@U3R1BTHB3>' GROUP BY to_user, award ORDER BY tval DESC;
 error_log('minipg get');
-        $stmt = $this->db->prepare('SELECT * FROM awards WHERE to_user = :user');
+        $stmt = $this->db->prepare('SELECT * FROM awards WHERE to_user = :user')->execute([':user' => $user]);
         //$stmt = $this->db->prepare("SELECT to_user, award, sum(value) as tval FROM awards where to_user = ? GROUP BY to_user, award ORDER BY tval DESC;");
         error_log('execute query');
 
-        $stmt->execute([':user' => $user]);
+        //$stmt->execute([':user' => $user]);
         $row = $stmt->fetchAll();
 error_log($row);
 
